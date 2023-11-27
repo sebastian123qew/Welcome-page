16867a2ecc10040024b4e5655181c9eb6a4b3fee
sebastian123qew <132944702+sebastian123qew@users.noreply.github.com>
2023-11-27 20:13:51 +0100

    update

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('enterButton').addEventListener('click', function () {
+    alert('Welcome! Enjoy your stay.');
+    // You can replace the alert with any other action you want to perform.
+});
